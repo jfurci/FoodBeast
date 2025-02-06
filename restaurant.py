8f21063a1b88406164b190148898e2cafa48a40a
Jack Furci <furci.j@northeastern.edu>
2025-02-05 21:53:19 -0500

    [FB-1] Create a restaurant class

--- restaurant.py
+++ restaurant.py
@@ -1,3 +1,4 @@
+import validators
 from typing import Optional
 
 class Restaurant:
@@ -16,7 +17,12 @@ class Restaurant:
         :param tags: List of words describing the restaurant.
         :param dress_code: The dress code, if any.
         :param notes: Any additional notes about the restaurant.
+        :raise Exception: If Google URL or name are empty, or if the URL isn't a valid URL.
         """
+        if "" in (google_url.strip(), name.strip()):
+            raise Exception("Must have non-empty Google URL and restaurant name.")
+        elif validators.url(google_url) is not True:
+            raise Exception("URL is not valid.")
         self._google_url = google_url
         self._name = name
         self._price_range = price_range
